e2fcfa190ce36f77a45b366942052747c2e09383
Felix Belau <felix.belau@tum.de>
2015-12-06 18:44:36 +0100

    final changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -29,27 +29,43 @@ var education = {
 			"degree" : "Bachelor of Science",
 			"majors" : "Physics",
 			"dates" : "2009-2011",
-			"url" : ""
+			"url" : "http://tum.de"
 		},
 		{
 			"name" : "Technische Universität München",
 			"location" : "Garching Forschungszentrum, Germany",
 			"degree" : "Bachelor of Science",
 			"majors" : "Computer Science",
-			"dates" : "2011-2014"
+			"dates" : "2011-2014",
+			"url" : "http://www.tum.de"
 		},
 		{
 			"name" : "Technische Universität München",
 			"location" : "Garching Forschungszentrum, Germany",
 			"degree" : "Master of Science",
 			"majors" : "Computer Science",
-			"dates" : "2014-2016"
+			"dates" : "2014-2016",
+			"url" : "http://www.tum.de"
 		}						
+	],
+	"onlinecourses" : [
+		{
+			"title" : "Front-End Web Developer Nanodegree",
+			"school" : "Udacity",
+			"date" : "2015-2016",
+			"url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		},
+		{
+			"title" : "iOS Developer Nanodegree",
+			"school" : "Udacity",
+			"date" : "2016",
+			"url" : "https://www.udacity.com/course/ios-developer-nanodegree--nd003"
+		}		
 	]
 }
 
 var projects = {
-	"project" : [{
+	"projects" : [{
 		"title" : "Fit Fuze iOS Application (iPhone & Apple Watch)",
 		"dates" : "2015", 
 		"description" : "Powerful fitness assistant with free Apple Watch support. Period. Leave your pen, papers and training diary in the locker room - FIT Fuze has everything you need. With no need in internet connection.",
@@ -77,7 +93,7 @@ var work = {
 			"title" : "iOS Developer",
 			"employer" : "HeKu IT",
 			"dates" : "2011-2015",
-			"description" : "I created really awesome ios Applications, like ReadingTrainer, Maxdome App and so on",
+			"description" : "I created really awesome ios Applications, like ReadingTrainer App and so on",
 			"location" : "Garching, Germany"		
 		}	
 	]
@@ -102,20 +118,33 @@ bio.display = function() {
 	var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
 	var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
 	var formattedSkype = HTMLcontactGeneric.replace("%contact%","skype").replace("%data%",bio.contacts.skype);
-	//$("#topContacts").append(formattedMobile,formattedEmail,formattedSkype,formattedLocation);
 	$("#footerContacts").append(formattedMobile,formattedEmail,formattedSkype, formattedLocation);
 };
 
 education.display = function() {
-	for(school in education.schools) {
+	for(schoolIndex in education.schools) {
 		$("#education").append(HTMLschoolStart);
-		
-		var formattedName = HTMLschoolName.replace("%data%",education.schools[school].name);
-		var formattedDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
-		var formattedDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
-		var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
-		var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[school].majors);
-		$(".education-entry:last").append(formattedName + formattedDegree,formattedDates,formattedLocation,formattedMajor);
+		var school = education.schools[schoolIndex];
+		var formattedName = HTMLschoolName.replace("%data%",school.name);
+		var formattedDegree = HTMLschoolDegree.replace("%data%",school.degree);
+		var formattedDates = HTMLschoolDates.replace("%data%",school.dates);
+		var formattedLocation = HTMLschoolLocation.replace("%data%",school.location);
+		var formattedMajor = HTMLschoolMajor.replace("%data%",school.majors);
+		var formattedUrl = HTMLonlineURL.replace("%data%",school.url);
+		$(".education-entry:last").append(formattedName + formattedDegree + formattedDates + formattedLocation + formattedMajor + formattedUrl);
+	}
+
+	if (education.onlinecourses.length > 0){
+		$("#education").append(HTMLonlineClasses);
+		for(onlineCourseIndex in education.onlinecourses) {
+			$("#education").append(HTMLschoolStart);
+			var onlinecourse = education.onlinecourses[onlineCourseIndex];
+			var formattedName = HTMLonlineTitle.replace("%data%",onlinecourse.title);
+			var formattedSchool = HTMLonlineSchool.replace("%data%",onlinecourse.school);
+			var formattedDates = HTMLonlineDates.replace("%data%",onlinecourse.date);
+			var formattedUrl = HTMLonlineURL.replace("%data%",onlinecourse.url);
+			$(".education-entry:last").append(formattedName + formattedSchool + formattedDates + formattedUrl);
+		}
 	}
 };
 
@@ -134,24 +163,29 @@ work.display = function(){
 }
 
 projects.display = function(){
-	for(item in projects.project){
+	for(projectIndex in projects.projects){
 		$("#projects").append(HTMLprojectStart);
-		var formattedTitle = HTMLprojectTitle.replace("%data%",projects.project[item].title);
-		var formattedDates = HTMLprojectDates.replace("%data%",projects.project[item].dates);
-		var formattedDescription = HTMLprojectDescription.replace("%data%",projects.project[item].description);
+		var project = projects.projects[projectIndex];
+		var formattedTitle = HTMLprojectTitle.replace("%data%",project.title);
+		var formattedDates = HTMLprojectDates.replace("%data%",project.dates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%",project.description);
 		
 		$(".project-entry:last").append(formattedTitle,formattedDates,formattedDescription);
-		for (image in projects.project[item].images) {
-			var formattedImage = HTMLprojectImage.replace("%data%",projects.project[item].images[image]);
+		for (image in project.images) {
+			var formattedImage = HTMLprojectImage.replace("%data%",project.images[image]);
 			$(".project-entry:last").append(formattedImage);
 		};
 	}
 };
 
 bio.display()
-work.display();
 education.display()
+work.display();
 projects.display()
+
+$(document).click(function(loc){
+	logClicks(loc.pageX,loc.pageY);
+});
 
 $("#mapDiv").append(googleMap);
 
